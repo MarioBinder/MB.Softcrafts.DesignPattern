21d7d7ffd4278d732970907a5de5d7bb5ec7c559
Mario Binder <mp@biggle.de>
2017-08-24 22:16:08 +0200

    Singleton Pattern

---
+++ MB.Softcrafts.DesignPattern/MB.Softcrafts.DesignPattern.Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MB.Softcrafts.DesignPattern.Singleton
+{
+
+    public sealed class Singleton2
+    {
+        private static readonly Lazy<Singleton2> _lazy =new Lazy<Singleton2>(() => new Singleton2());
+
+        public static Singleton2 Instance => _lazy.Value;
+
+        private Singleton2()
+        {
+        }
+    }
+
+
+    public class Singleton1
+    {
+        private static Singleton1 _instance;
+        private Singleton1()
+        {
+        }
+        public static Singleton1 Instance => _instance ?? (_instance = new Singleton1());
+    }
+}
